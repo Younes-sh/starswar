2e04c635104082507ab993a32b01a5737d97836b
younes sheikhlar <sheikhlaryounes@mail.com>
2022-05-09 17:05:32 +0200

    created shuttle information section Using HTML CSS and JS

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,25 @@ function sideHandler(){
     sidebar.classList.toggle('showSidebar')
     sidebar.classList.toggle('hideSidebar')
 }
+//          Shuttle 
+
+
+//          info shuttle !!!
+
+const more_info = document.querySelector('#more-info')
+const infoShuttle = document.querySelector('.information_shuttle');
+more_info.addEventListener('click' , more_infoHandler)
+function more_infoHandler(){
+    infoShuttle.classList.toggle('show_infoShuttle');
+    infoShuttle.classList.toggle('hight_info_shuttle')
+}
+
+// close information shuttle 
+const closeInfo = document.querySelector('#close-info');
+closeInfo.addEventListener('click',closeInfoHandler);
+function closeInfoHandler(){
+    infoShuttle.classList.toggle('show_infoShuttle');
+    infoShuttle.classList.toggle('hight_info_shuttle')
+}
+
+//                  type text info shuttle
